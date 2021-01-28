6f6edfe7c33e58df0ee395d005a98b3e30e0946e
Song-YunMin <thd930308@naver.com>
2021-01-28 23:23:28 +0900

    [UPDATE] todo class 수정

--- Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Todos.js
+++ Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Todos.js
@@ -1,12 +1,52 @@
 class Todos {
     #TODOS_LS
+    #dom            // todo List
+    #todoForm
+
     constructor() {
         this.#TODOS_LS = 'toDos';
-        const loadedToDos = localStorage.getItem(this.#TODOS_LS);
+        this.prepareDom();
+        this.startForm();
+        this.hasLocalStorage();
+        this.getInputText();
     }
 
-    saveToDos(){
+    prepareDom() {
+        const todosContainer = document.querySelector('.todosContainer');
+        this.#dom = todosContainer.querySelector('.todos');
+        this.#todoForm = todosContainer.querySelector('.inputTodos');
+    }
 
+    startForm() {
+        this.#todoForm.addEventListener("submit", this.getInputText);
+    }
+
+    // form에 있는 Text를 받아서 setTodo로 전달
+    getInputText(event) {
+        event.preventDefault();
+        const text = this.#dom.value;
+        this.setTodoList(text);
+        console.log(text);
+        debugger;
+        this.#dom.value = "";
+    }
+
+    hasLocalStorage() {
+        const loadedToDos = localStorage.getItem(this.#TODOS_LS);
+        if (loadedToDos !== null) {               // 데이터가 있다면?
+            const parsedToDos = JSON.parse(loadedToDos);
+            parsedToDos.forEach((toDo) => {
+                console.log(toDo);
+                debugger;
+                this.setTodoList(toDo);
+            });
+        }
+    }
+
+    setTodoList(text) {
+        const list = document.createElement("li");
+        const delBt = document.createElement("button");
+        const span = document.createElement("span");
     }
 
 
