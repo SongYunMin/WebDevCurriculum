fb9ff52e4b53c7bd4d43067219eed9d47241bd4c
Song-YunMin <thd930308@naver.com>
2021-01-28 18:52:35 +0900

    [UPDATE] todo class 생성

--- Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Todos.js
+++ Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Todos.js
@@ -1,6 +1,13 @@
 class Todos {
+    #TODOS_LS
     constructor() {
+        this.#TODOS_LS = 'toDos';
+        const loadedToDos = localStorage.getItem(this.#TODOS_LS);
+    }
 
+    saveToDos(){
 
     }
+
+
 }
