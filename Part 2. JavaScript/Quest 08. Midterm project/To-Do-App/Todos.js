a45d3e22dac28e41b871674e20a0a90c3a0ba276
Song-YunMin <thd930308@naver.com>
2021-01-29 13:44:25 +0900

    [UPDATE] Todo list 기능 수정

--- Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Todos.js
+++ Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Todos.js
@@ -2,44 +2,52 @@ class Todos {
     #TODOS_LS
     #dom            // todo List
     #todoForm
+    #todoInput
 
     constructor() {
         this.#TODOS_LS = 'toDos';
         this.prepareDom();
         this.startForm();
-        this.hasLocalStorage();
-        this.getInputText();
+        // this.hasLocalStorage();
     }
 
     prepareDom() {
         const todosContainer = document.querySelector('.todosContainer');
         this.#dom = todosContainer.querySelector('.todos');
-        this.#todoForm = todosContainer.querySelector('.inputTodos');
+        this.#todoForm = document.querySelector('.inputTodosForm');
+        this.#todoInput = this.#todoForm.querySelector('.inputTodos');
     }
 
     startForm() {
-        this.#todoForm.addEventListener("submit", this.getInputText);
+        console.log(this.#todoForm);
+        this.#todoForm.addEventListener("submit", e =>{
+            e.preventDefault();
+            console.log("Event!!");
+            const text = this.#todoInput.value;
+            // this.setTodoList(text);
+            console.log(text);
+            this.#dom.value = "";
+            // this.getInputText();
+        });
     }
 
     // form에 있는 Text를 받아서 setTodo로 전달
-    getInputText(event) {
-        event.preventDefault();
-        const text = this.#dom.value;
-        this.setTodoList(text);
-        console.log(text);
-        debugger;
-        this.#dom.value = "";
+    getInputText() {
+        // const text = this.#dom.value;
+        // // this.setTodoList(text);
+        // console.log(text);
+        // debugger;
+        // this.#dom.value = "";
     }
 
     hasLocalStorage() {
         const loadedToDos = localStorage.getItem(this.#TODOS_LS);
         if (loadedToDos !== null) {               // 데이터가 있다면?
             const parsedToDos = JSON.parse(loadedToDos);
-            parsedToDos.forEach((toDo) => {
-                console.log(toDo);
-                debugger;
-                this.setTodoList(toDo);
-            });
+            // parsedToDos.forEach((toDo) => {
+            //     console.log(toDo);
+            //     // this.setTodoList(toDo);
+            // });
         }
     }
 
